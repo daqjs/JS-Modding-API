a8923c637bff1312393da0047018f010bed6727e
xDark <aleshkailyashevich@gmail.com>
2019-06-11 12:00:22 +0300

    CPS show script

---
+++ cps.js
@@ -0,0 +1,13 @@
+Events.on('KEY_PRESS', function (ref) {
+    Client.sendMessageToPlayer('Key: ' + ref.value)
+}, EventPriority.NORMAL)
+let clicks = 0
+JSEngine.setInterval(function() {clicks = 0}, 1000)
+Events.on('MOUSE_PRESS', function (ref) {
+    if (ref.value == 0) {
+        clicks++;
+    }
+}, EventPriority.NORMAL)
+Events.on('GUI_DRAW', function() {
+    Draw.drawString('CPS: ' + clicks, 4, 13, 0xffffffff)
+}, EventPriority.NORMAL)
